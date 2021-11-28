1a202c1b17f6da693e9c58c3f1fdf6b3edd0d8aa
bulpil <bulpil@abv.bg>
2021-11-28 21:50:34 +0200

    strings

---
+++ app.js
@@ -0,0 +1,33 @@
+// Number
+console.log('Max integer, from constant: ' + Number.MAX_SAFE_INTEGER)
+console.log('Max integer, calculated: ' + (Math.pow(2, 53) - 1))
+console.log('Min integer, from constant: ' + Number.MIN_SAFE_INTEGER)
+
+const cars = new Array('Mazda', 'Mercedes', 'Ford')
+const trucks = ['Volvo', 'Tatra', 'MAN']
+
+for (let i = 0; i < cars.length; i++) {
+    const car = cars[i]
+    console.log(car)
+}
+
+for (let truck of trucks) {
+    console.log(truck)
+}
+
+console.log(0.4 + 0.2)
+console.log(parseFloat((0.4 + 0.2).toFixed(1)))
+console.log(+(0.4+0.2).toFixed(1))
+
+
+//Strings
+const name = 'Fluf'
+const age = 43
+
+function getAge(){
+    return age
+}
+
+const output = `Hello, my name is ${name} and my age is ${getAge()}.`
+console.log(output)
+console.log(`My name is ${name.length} letters long.` )
